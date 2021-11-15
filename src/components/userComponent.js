14e8ae04c53026e336db91de928987e70cdb43e1
yigitalidev <yigitaliarslan27@gmail.com>
2021-11-15 20:59:07 +0300

    3rd day

---
+++ src/components/userComponent.js
@@ -0,0 +1,20 @@
+
+import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
+import User from "../models/user.js"
+import UserService from "../services/userService.js"
+
+console.log("User component yüklendi")
+
+let logger1 = new MongoLogger()
+let userService = new UserService(logger1)
+
+
+
+let user1 = new User(1,"Yiğit","Ali","İstanbul")
+let user2 = new User(2,"Engin","Demiroğ","Ankara")
+userService.add(user1)
+userService.add(user2)
+
+console.log(userService.list())
+console.log(userService.getById(2))
+
